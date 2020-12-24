363761d54d36a27b8abf2ac4a36f0676c3749021
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-12-24 23:29:33 +0100

    change base provider function name

--- src/providers/ion-bottom-sheet-provider.ts
+++ src/providers/ion-bottom-sheet-provider.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class IonBottomSheetProvider {
-    reasonToJoin(){
+    ibsProvider(){
         return 'The Ionic Bootom Sheet Component';
     }
 }
