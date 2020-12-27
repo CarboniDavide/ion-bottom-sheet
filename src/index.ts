e049f9743f7de9eae464d840ea6e17b8d32d2cef
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2020-12-27 23:19:38 +0100

    auto gen commit

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ion-bottom-sheet.module';
-export * from './components/ion-bottom-sheet.component';
-export * from './providers/ion-bottom-sheet-provider';
-export * from './components/ion-bottom-sheet-state';
--- src/providers/ion-bottom-sheet-provider.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class IonBottomSheetProvider {
-    ibsProvider(){
-        return 'The Ionic Bootom Sheet Component';
-    }
-}
